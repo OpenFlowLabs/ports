03fe7a057131fcf4f91b7727f9f66b05eb2770ba
Till Wegmueller <toasterson@gmail.com>
2021-03-02 23:53:31 -0300

    Playing with the parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "specfile.pest"]
+struct SpecFile;
+
 fn main() {
-    println!("Hello, world!");
+    let specfile = r#"Name:       hello-world
+Version:    1
+Release:    1
+Summary:    Most simple RPM package
+License:    FIXME
+
+%description
+This is my first RPM package, which does nothing.
+
+%prep
+# we have no source, so nothing here
+
+%build
+cat > hello-world.sh <<EOF
+#!/usr/bin/bash
+echo Hello world
+EOF
+
+%install
+mkdir -p %{buildroot}/usr/bin/
+install -m 755 hello-world.sh %{buildroot}/usr/bin/hello-world.sh
+
+%files
+/usr/bin/hello-world.sh
+
+%changelog
+# let's skip this for now"#;
+
+    let pairs = SpecFile::parse(Rule::file, specfile).unwrap_or_else(|e| panic!("{}", e));
+
+    // Because ident_list is silent, the iterator will contain idents
+    for pair in pairs {
+        // A pair is a combination of the rule which matched and a span of input
+        println!("Rule:    {:?}", pair.as_rule());
+        println!("Span:    {:?}", pair.as_span());
+        println!("Text:    {}", pair.as_str());
+
+        // A pair can be converted to an iterator of the tokens which make it up:
+        for inner_pair in pair.into_inner() {
+            match inner_pair.as_rule() {
+                Rule::file => println!("File:  {}", inner_pair.as_str()),
+                Rule::variable => println!("Variable:   {}", inner_pair.as_str()),
+                _ => println!("Unknown:   {}", inner_pair.as_str()),
+            };
+        }
+    }
 }
